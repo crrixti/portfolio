a8dce1560468de982b7aec7082c0076306240b14
crrixti <cristigare@gmail.com>
2025-04-24 19:20:04 +0200

    portfolio

--- js/funciones.js
+++ js/funciones.js
@@ -1,18 +1,17 @@
-const etiquetas = document.querySelectorAll(".etiquetas");
-const anyo = document.querySelectorAll(".anyo");
+const titulo = document.querySelector("h1");
+const header = document.querySelector("header");
+const h3 = document.querySelector("h3");
 
-//poner con un forEach, que cada vez que esté en hover el contenedor, a la clase etiqueta y anyo se les pone la clase .escondido-hov
-const titulo = document.querySelector("h2");
+titulo.innerHTML = titulo.innerText.split("").map( c => {
+    return c != " " ? `<span>${c}</span>` : c;
+}).join("");
 
-        titulo.innerHTML = titulo.innerText.split("").map( c => {
-            return c != " " ? `<span>${c}</span>` : c;
-        }).join("");
+const letras = document.querySelectorAll("h1 span");
 
-        const letras = document.querySelectorAll("h1 span");
+for (let i = 0; i <letras.length; i++){
+    setTimeout(() => {
+        letras[i].classList.add("visible");
+    }, 100 * i);
+}
 
-        for (let i = 0; i <letras.length; i++){
-            setTimeout(() => {
-                letras[i].classList.add("visible");
-                //si quiero poner otra animación después de esta, la pongo aquí
-            }, 300 * i);
-        }
+
